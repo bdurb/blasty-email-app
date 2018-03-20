94e8dd047e40706eb7ff4ceaae8d3478c250eac2
bdurb <brian@rpmgreenville.com>
2018-03-19 22:13:56 -0400

    more refactoring of passport

--- services/passport.js
+++ services/passport.js
@@ -27,11 +27,10 @@ passport.use(
     async (accessToken, refreshToken, profile, done) => {
       const existingUser = await User.findOne({ googleId: profile.id })
       if (existingUser) {
-        done(null, existingUser);
-      } else {
-        const user = await new User({ googleId: profile.id }).save()
-        done(null, user);
+        return done(null, existingUser);
       }
+      const user = await new User({ googleId: profile.id }).save()
+      done(null, user);
     }
   )
 );
