4991869e2f6744f979a9b36a2b1099082c3126af
bdurb <brian@rpmgreenville.com>
2018-03-21 20:26:32 -0400

    began styling for landing page

---
+++ client/src/components/Landing.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const landing = () => {
+  return (
+    <div>
+      <h1>
+        Blasty Feedback
+      </h1>
+      Collect feedback from your users
+    </div>
+  );
+};
