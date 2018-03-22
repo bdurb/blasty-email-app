4f725c13f54793c83a032a220a71e18ed4b42f35
bdurb <brian@rpmgreenville.com>
2018-03-21 20:23:39 -0400

    set up header with authentication

--- client/src/components/Header.js
+++ client/src/components/Header.js
@@ -1,6 +1,20 @@
 import React, { Component } from 'react';
+import { connect } from 'react-redux';
 
 class Header extends Component {
+  renderContent() {
+    switch (this.props.auth) {
+      case null:
+        return;
+      case false:
+        return (
+          <li><a href="/auth/google">Login With Google</a></li>
+        );
+      default:
+        return <li><a href="/api/logout">Logout</a></li>
+    }
+  }
+
   render() {
     return (
       <nav>
@@ -9,9 +23,7 @@ class Header extends Component {
             Blasty
           </a>
           <ul className="right">
-            <li>
-              <a>Login With Google</a>
-            </li>
+            {this.renderContent()}
           </ul>
         </div>
       </nav>
@@ -19,4 +31,8 @@ class Header extends Component {
   }
 }
 
-export default Header;
+const mapStateToProps = ({ auth }) => {
+  return { auth };
+}
+
+export default connect(mapStateToProps)(Header);
