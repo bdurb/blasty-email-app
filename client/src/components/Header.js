2144078c9c1a4cf835a7deb5c3e89f2a583f8106
bdurb <brian@rpmgreenville.com>
2018-03-22 00:59:36 -0400

    set up payment button on header

--- client/src/components/Header.js
+++ client/src/components/Header.js
@@ -1,6 +1,7 @@
 import React, { Component } from 'react';
 import { connect } from 'react-redux';
 import { Link } from 'react-router-dom';
+import Payments from './Payments';
 
 class Header extends Component {
   renderContent() {
@@ -12,7 +13,10 @@ class Header extends Component {
           <li><a href="/auth/google">Login With Google</a></li>
         );
       default:
-        return <li><a href="/api/logout">Logout</a></li>
+        return [
+          <li key="1"><Payments /></li>,
+          <li key="2"><a href="/api/logout">Logout</a></li>
+        ];
     }
   }
 
