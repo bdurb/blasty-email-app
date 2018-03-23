1b083e60f54419be78264918fb2edc04950133f1
bdurb <brian@rpmgreenville.com>
2018-03-23 00:33:30 -0400

    setting up billing through stripe

--- client/src/actions/index.js
+++ client/src/actions/index.js
@@ -8,3 +8,11 @@ export const fetchUser = () => async dispatch => {
     payload: res.data
   });
 };
+
+export const handleToken = (token) => async dispatch => {
+  const res = await axios.post('/api/stripe', token)
+  dispatch({
+    type: FETCH_USER,
+    payload: res.data
+  });
+};
