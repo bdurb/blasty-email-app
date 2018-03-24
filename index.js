d8a36d40a7844045c5011a254cf0980c737cfd13
bdurb <brian@rpmgreenville.com>
2018-03-24 01:37:55 -0400

    testing

--- index.js
+++ index.js
@@ -26,9 +26,9 @@ require('./routes/billingRoutes')(app);
 
 if (process.env.NODE_ENV === 'production') {
   app.use(express.static('client/build'));
-  const path = require('path');
+  const thepath = require('thepath');
   app.get('*', (req, res) => {
-    res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'));
+    res.sendFile(thepath.resolve(__dirname, 'client', 'build', 'index.html'));
   });
 }
 
