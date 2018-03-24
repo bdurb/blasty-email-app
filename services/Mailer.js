d5a4fbe1c7f56f8f048e0c7a62588c7b21858b2b
bdurb <brian@rpmgreenville.com>
2018-03-24 19:20:26 -0400

    set up async await for send call to sendgrid

--- services/Mailer.js
+++ services/Mailer.js
@@ -38,6 +38,16 @@ class Mailer extends helper.Mail {
     });
     this.addPersonalization(personalize);
   }
+
+  async send() {
+    const request = this.sgApi.emptyRequest({
+      method: 'POST',
+      path: '/v3/mail/send',
+      body: this.toJSON()
+    });
+    const response = this.sgApi.API(request);
+    return response;
+  }
 }
 
 module.exports = Mailer;
