e88d1029ed1ff4ef0536532773f0f903f6d531ae
bdurb <brian@rpmgreenville.com>
2018-03-24 19:16:28 -0400

    added api key for sendgrid mailer

--- services/Mailer.js
+++ services/Mailer.js
@@ -6,6 +6,7 @@ class Mailer extends helper.Mail {
   constructor({ subject, recipients }, content) {
     super();
     
+    this.sgApi = sendgrid(keys.sendGridKey);
     this.from_email = new helper.Email('no-reply@blasty.com');
     this.subject = subject;
     this.body = new helper.Content('text/html', content);
