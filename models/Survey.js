786b825b36467eb4af1635022bbfe643a72d2d70
bdurb <brian@rpmgreenville.com>
2018-03-24 14:09:41 -0400

    set up survey reference to user

--- models/Survey.js
+++ models/Survey.js
@@ -14,7 +14,13 @@ const surveySchema = new Schema({
   no: {
     type: Number,
     default: 0
-  }
+  },
+  _user: {
+    type: Schema.Types.ObjectId,
+    ref: 'User'
+  },
+  dateSent: Date,
+  lastResponded: Date
 });
 
 mongoose.model('surveys', surveySchema);
