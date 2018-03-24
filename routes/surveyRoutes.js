fcf72921bc3600cf432da1e55567918d0d337a84
bdurb <brian@rpmgreenville.com>
2018-03-24 17:38:52 -0400

    set up mailer class

--- routes/surveyRoutes.js
+++ routes/surveyRoutes.js
@@ -1,6 +1,8 @@
 const mongoose = require('mongoose');
 const requireLogin = require('../middlewares/requireLogin');
 const requireCredits = require('../middlewares/requireCredits');
+const Mailer = require('../services/Mailer');
+const surveyTemplate = require('../services/emailTemplates/surveyTemplate');
 
 const Survey = mongoose.model('surveys');
 
@@ -14,6 +16,7 @@ module.exports = app => {
       recipients: recipients.split(',').map(email => ({ email: email.trim() })),
       _user: req.user.id,
       dateSent: Date.now()
-    })
+    });
+    const mailer = new Mailer(survey, surveyTemplate(survey));
   });
 };
