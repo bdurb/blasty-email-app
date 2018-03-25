532c8030f2ae178511975e0e0fea45a424c0b96f
bdurb <brian@rpmgreenville.com>
2018-03-24 20:04:06 -0400

    finished mailer and confirmed functionality with axios

--- client/src/index.js
+++ client/src/index.js
@@ -8,6 +8,7 @@ import reduxThunk from 'redux-thunk';
 import App from './components/App';
 import reducers from './reducers';
 
+
 const store = createStore(reducers, {}, applyMiddleware(reduxThunk));
 
 ReactDOM.render(
