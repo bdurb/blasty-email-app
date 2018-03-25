396f28c0214be291562bed02dcd96ec6f3dc45b8
bdurb <brian@rpmgreenville.com>
2018-03-25 13:47:02 -0400

    added redux form to survey form component

--- client/src/components/surveys/SurveyForm.js
+++ client/src/components/surveys/SurveyForm.js
@@ -1,4 +1,5 @@
 import React, { Component } from 'react';
+import{ reduxForm } from 'redux-form';
 
 class SurveyForm extends Component {
   render() {
@@ -10,4 +11,6 @@ class SurveyForm extends Component {
   }
 }
 
-export default SurveyForm;
+export default reduxForm({
+  form: 'surveyForm'
+})(SurveyForm);
