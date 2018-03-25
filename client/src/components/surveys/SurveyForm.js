cf7b6960b892ded90d194ad99f4b76f958c1335e
bdurb <brian@rpmgreenville.com>
2018-03-25 14:16:49 -0400

    setting up survey with redux form

--- client/src/components/surveys/SurveyForm.js
+++ client/src/components/surveys/SurveyForm.js
@@ -1,11 +1,22 @@
 import React, { Component } from 'react';
-import{ reduxForm } from 'redux-form';
+import{ reduxForm, Field } from 'redux-form';
+import SurveyField from './SurveyField';
 
 class SurveyForm extends Component {
+  renderFields() {
+    return (
+      <div>
+        <Field type="text" name="title" component={SurveyField} />
+      </div>
+    )
+  }
   render() {
     return (
       <div>
-        SurveyForm!
+        <form onSubmit={this.props.handleSubmit(values => console.log(values))}>
+          {this.renderFields()}
+          <button type="submit">Submit</button>
+        </form>
       </div>
     );
   }
