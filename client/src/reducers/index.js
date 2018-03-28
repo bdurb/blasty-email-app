c2f8b9e3d8ef66955aa082344728a069a0da5dd5
bdurb <brian@rpmgreenville.com>
2018-03-27 22:16:29 -0400

    set up surveys reducer

--- client/src/reducers/index.js
+++ client/src/reducers/index.js
@@ -1,8 +1,10 @@
 import { combineReducers } from 'redux';
 import { reducer as reduxForm } from 'redux-form';
 import authReducer from './authReducer';
+import surveysReducer from 'surveyReducer';
 
 export default combineReducers({
   auth: authReducer,
-  form: reduxForm
+  form: reduxForm,
+  surveys: surveysReducer
 });
