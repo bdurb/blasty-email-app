49b8234bcacd5de5d645d3f027b04a6aca055659
bdurb <brian@rpmgreenville.com>
2018-03-27 22:34:09 -0400

    fixed some naming conventions

--- client/src/components/Dashboard.js
+++ client/src/components/Dashboard.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
-import SurveyList from './surveyList';
+import SurveyList from './surveys/SurveyList';
 
 const Dashboard = () => {
   return (
--- client/src/components/surveys/SurveyList.js
+++ client/src/components/surveys/SurveyList.js
@@ -2,7 +2,10 @@ import React, { Component } from 'react';
 import { connect } from 'react-redux';
 import { fetchSurveys } from '../../actions';
 
-class SurveyList extends React {
+class SurveyList extends Component {
+  componentDidMount() {
+    this.props.fetchSurveys();
+  }
   render() {
     return (
       <div>
